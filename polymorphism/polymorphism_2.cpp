cfeb569537adaf8499f48436ed95ac6ace14331d
lanma <lanmarock@gmail.com>
2025-09-28 15:07:07 +0700

    initial commit

--- polymorphism/polymorphism_2.cpp
+++ polymorphism/polymorphism_2.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 using namespace std;
 
+class Piece {
+    protected:
+        string from;
+    public:
+        Piece(const string& start) : from(start) {}
+        virtual ~Piece() {}
 
+        virtual bool canMove(string& to) 
+            const = 0;
+
+        const string& startSquare() const { return from; }
+    };
+
+class Man : public Piece {
+    public:
+    // Man:canMoveRo using the man rule
+};
+
+class King : public Piece {
+    public:
+    // King:canMoveRo using the man rule
+};
 
 int main () {
     return 0;
