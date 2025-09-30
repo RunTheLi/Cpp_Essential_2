cf6c83e9f81bfb6136a3c6016af26e1d6d1f58c5
lanma <lanmarock@gmail.com>
2025-09-30 12:49:45 +0700

    Initial commit

--- polymorphism/polymorphism_3.cpp
+++ polymorphism/polymorphism_3.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <sstream>
+#include <string>
+
 using namespace std;
 
+class IPAdress {
+    private:
+        string value;
+    public:
+        IPAddress(string ip);
+        IPAddress(const IPAddress &other);
+        
+        void print();
+};
 
+class IPAddressChecked : public IPAdress {
+    private:
+        bool isCorrect;
+    public:
+
+};
 
 int main () {
+
     return 0;
 }
