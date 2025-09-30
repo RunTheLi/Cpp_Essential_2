a558e957821215e148d37ab2615018387d449243
lanma <lanmarock@gmail.com>
2025-09-30 12:33:52 +0700

    initial commit

--- polymorphism/polymorphism_2.cpp
+++ polymorphism/polymorphism_2.cpp
@@ -1,29 +1,72 @@
 #include <iostream>
+#include <string>
+#include <vector>
+
 using namespace std;
 
 class Piece {
-    protected:
-        string from;
-    public:
-        Piece(const string& start) : from(start) {}
-        virtual ~Piece() {}
+protected:
+    pair<int,int> parsePosition(string pos) {
+        int col = pos[0] - 'a' + 1;
+        int row = pos[1] - '0';
+        return {col, row};
+    }
 
-        virtual bool canMove(string& to) 
-            const = 0;
-
-        const string& startSquare() const { return from; }
-    };
+public:
+    virtual bool checkMove(string from, string to) = 0;
+    virtual ~Piece() {}
+};
 
 class Man : public Piece {
-    public:
-    // Man:canMoveRo using the man rule
+public:
+bool checkMove(string from, string to) override {
+    pair<int,int> fromCoord = parsePosition(from);
+    pair<int,int> toCoord = parsePosition(to);
+
+    int deltaRow = toCoord.second - fromCoord.second;
+    int deltaCol = toCoord.first - fromCoord.first;
+
+        if (deltaRow == 1 && abs(deltaCol == 1)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 };
 
 class King : public Piece {
-    public:
-    // King:canMoveRo using the man rule
+public:
+bool checkMove(string from, string to) override {
+    pair<int,int> fromCoord = parsePosition(from);
+    pair<int,int> toCoord = parsePosition(to);
+
+    int deltaRow = toCoord.second - fromCoord.second;
+    int deltaCol = toCoord.first - fromCoord.first;
+
+        if (abs(deltaRow) == abs(deltaCol)) {
+            return true;
+        } else {
+            return false;
+        }   
+    }
 };
 
+
 int main () {
+    Piece* piece;
+
+    piece = new (Man);
+    cout << boolalpha << piece->checkMove("b1", "c2") << endl; 
+    delete piece;
+
+    piece = new (King);
+    cout << boolalpha << piece->checkMove("b1", "d3") << endl; 
+    delete piece;
+    
+    piece = new (Man);
+    cout << boolalpha << piece->checkMove("b1", "d3") << endl; 
+    delete piece;
+
     return 0;
 }
+
